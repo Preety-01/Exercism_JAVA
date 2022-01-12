5d5fd973a8e9e3a97efcca91770c25d873c8a1e0
Preety-01 <prmehra.16@gmail.com>
2022-01-12 15:14:33 +0530

    Log-Levels

---
+++ log-levels/src/main/java/LogLevels.java
@@ -0,0 +1,37 @@
+public class LogLevels {
+        public static String message(String logLine) {
+        String words[]=logLine.split("\\W+");
+        String answer="";
+        for(int i=2;i<words.length-1;i++)
+        {
+            answer+=words[i]+" ";
+        }
+        answer+=words[words.length-1];
+        return answer;
+    }
+
+    public static String logLevel(String logLine) {
+        if(logLine.contains("[ERROR]"))
+            return "error";
+        else if(logLine.contains("[WARNING]"))
+            return "warning";
+        return "info";
+    }
+
+    public static String reformat(String logLine) {
+        String words[]=logLine.split("\\W+");
+        String answer="";
+        for(int i=2;i<words.length;i++)
+        {
+            answer+=words[i] + " ";
+        }
+        if(logLine.contains("[ERROR]"))
+             answer+="(error)";
+        else if(logLine.contains("[WARNING]"))
+            answer+="(warning)";
+        else
+            answer+="(info)";
+        
+        return answer;
+    }
+}
