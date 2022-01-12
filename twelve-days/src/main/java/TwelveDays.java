c524afe40b510f2ddc6f185916171cefee62c152
Preety-01 <prmehra.16@gmail.com>
2022-01-12 12:06:39 +0530

    Twelve-Days

--- twelve-days/src/main/java/TwelveDays.java
+++ twelve-days/src/main/java/TwelveDays.java
@@ -1,13 +1,60 @@
 class TwelveDays {
+    String[] days = {"first", "second", "third","fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth"};
+    
+    String[] message = {" a Partridge in a Pear Tree.\n", " two Turtle Doves,", " three French Hens,", " four Calling Birds,", " five Gold Rings,", " six Geese-a-Laying,", " seven Swans-a-Swimming,", " eight Maids-a-Milking,", " nine Ladies Dancing,", " ten Lords-a-Leaping,", " eleven Pipers Piping,", " twelve Drummers Drumming,"};
+    
     String verse(int verseNumber) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        String initialString = "On the " + days[verseNumber-1] + " day of Christmas my true love gave to me:";
+        for(int i=verseNumber;i>0;i--)
+        {
+            if(i==1 && verseNumber>1)
+                initialString+=" and"+message[i-1];
+            else
+                initialString+=message[i-1];
+        }
+        return initialString;
     }
 
     String verses(int startVerse, int endVerse) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        String finalString="";
+        for(int i=startVerse;i<=endVerse;i++)
+        {
+            String initialString="";
+            initialString+="On the " + days[i-1] + " day of Christmas my true love gave to me:";
+            for(int j=i;j>0;j--)
+            {
+                if(j==1 && i>1)
+                    initialString+=" and"+message[j-1];
+                else
+                    initialString+=message[j-1];
+            }                 
+            if(i!=endVerse)
+                finalString+=initialString + "\n";
+            else
+                finalString+=initialString;
+        }
+        return finalString;
     }
     
     String sing() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        String finalString="";
+        for(int i=1;i<=12;i++)
+        {
+            String initialString="";
+            initialString+="On the " + days[i-1] + " day of Christmas my true love gave to me:";
+            for(int j=i;j>0;j--)
+            {
+                if(j==1 && i>1)
+                    initialString+=" and"+message[j-1];
+                else
+                    initialString+=message[j-1];
+            }       
+            if(i!=12)
+                finalString+=initialString + "\n";
+            else
+                finalString+=initialString;
+        }
+        return finalString;
     }
 }
+
